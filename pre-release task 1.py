3209454be42d7a27a6e9645b71f24a5fe543a642
adenchoy <adenchoy8@gmail.com>
2020-02-16 23:25:18 +0800

    done

--- pre-release task 1.py
+++ pre-release task 1.py
@@ -1,26 +1,36 @@
-day = 5
-time=  0
-hourstay = 0
-price=0
-while True:
+day = 0
+time =  0
+hourstay = 314134
+price = 0
+check = ()
+multipliers = [10,10,10,10,10,3,2]
+maxstay = [2,2,2,2,2,4,8]
+def modcheck(freqcheck):
+    if 11-((int(freqcheck[0])*5+int(freqcheck[1])*4+int(freqcheck[2])*3+int(freqcheck[3])*2)%11) == int(freqcheck[4]):
+        check = 'y'
+    return check
+while day > 7 or day < 1:
     day = int(input('what is the day? 1= monday 7=sunday'))
-    if day >7 or day < 1:
-        print('error')
+while time < 7 or time > 24:
+    time = int(input('what is the arrival time'))
+while True:
+    if hourstay + time < 24 and hourstay <= maxstay[day-1]:
+        break
+    hourstay = int(input('how long are you intending to stay?'))
+if time<15:
+    price = hourstay*multipliers[day-1]
+else:
+    price = hourstay*2
+freq = input('Do you have a frequent parking number?')
+if freq in ['Yes','y','yes','Y']:
+    freqcheck = input('Please input your frequent parking number.')
+    check = modcheck(freqcheck)
+if check == 'y':
+    if time<=15:
+        price = price*0.9
+        print(price)
     else:
-        while True:
-            time = int(input('what is the arrival time'))
-            if time > 0 and time <24:
-                break
-    break
-if day >=1 and day <=5:
-    while True:
-        hourstay = int(input('how long are you intending to stay?'))
-        if hourstay + time <= 24:
-            break
-    if time>15:
-        price= hourstay*10
-    else:
-        price =hourstay*2
-print(price)
-    
-    
+        price = price*0.5
+        print(price)
+else:
+    print(price)
