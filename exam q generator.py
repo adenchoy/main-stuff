bf9f85b550d0f6f9e7eb78ece0dca94aab608039
adenchoy <adenchoy8@gmail.com>
2020-08-14 12:24:52 +0800

    done

---
+++ exam q generator.py
@@ -0,0 +1,4 @@
+import random
+command = ['Explain', "Define", "Outline", "State"]
+meme = ['selection','iteration', 'sequence','abstraction','stepwise refinement','decomposition']
+print(command[random.randint(0,3)],meme[random.randint(0,5)])
