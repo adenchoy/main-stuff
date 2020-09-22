9eaaafb878d9fa4d9d7d593495aa9292a79fd1aa
adenchoy <adenchoy8@gmail.com>
2020-09-22 13:53:58 +0800

    done

--- AS75.py
+++ AS75.py
@@ -2,4 +2,3 @@ denary = input("input denary number")
 bcdlist =[]
 for i in range(int(denary)):
     bcdlist.append(bin(int(denary[i+1])))
-    
---
+++ TU014.py
@@ -0,0 +1,14 @@
+MyString = "abcdef"
+
+print(MyString)
+print(MyString[0:3])
+print(MyString[3::])
+print(MyString[0:6:2])
+print(MyString[-1:7:-1])
+print(MyString[0:-2])
+
+StringSlice = 'league of legends'
+print(StringSlice)
+print(StringSlice[0::2])
+print(StringSlice[::-1])
+
