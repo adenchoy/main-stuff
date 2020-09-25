8fd54fd5b1c545256f4f088e345b69e6e7e0f167
adenchoy <adenchoy8@gmail.com>
2020-09-25 12:45:07 +0800

    done

--- pseudocode functions.py
+++ pseudocode functions.py
@@ -56,3 +56,42 @@ def ASC(char):
 # example function being called    
 my_string = LEFT("hello",2)
 print(my_string)
+
+print('1, left')
+print('2, right')
+print('3, mid')
+print('4, length')
+print('5, lcase')
+print('6, ucase')
+print('7, to upper')
+print('8, to lower')
+print('9, num to string')
+print('10, string to num')
+print('11, int')
+print('12, asc')
+
+x,y,z,a = input('input which function you would like and which parameters would you like to pass through (num, param1, param2, param3) input 0s if param not needed').split()
+if x == 1:
+    print(LEFT(string(y),z))
+elif x == 2:
+    print(RIGHT(y,z))
+elif x == 3:
+    print(MID(y,z,a))
+elif x == 4:
+    print(LENGTH(y))
+elif x == 5:
+    print(LCASE(y))
+elif x == 6:
+    print(UCASE(y))
+elif x == 7:
+    print(TO_UPPER(y))
+elif x == 8:
+    print(TO_LOWER(y))
+elif x == 9:
+    print(NUM_TO_STRING(y))
+elif x == 10:
+    print(STRING_TO_NUM(y))
+elif x == 11:
+    print(INT(y))
+elif x == 12:
+    print(ASC(y))
