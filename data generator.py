904ff768177fe6bb379b951aa1fa1c0c017d33ec
adenchoy <adenchoy8@gmail.com>
2020-09-29 13:55:54 +0800

    done

--- data generator.py
+++ data generator.py
@@ -11,4 +11,9 @@ def realgen():
 def boolgen():
     return bool(random.getrandbits(1))
 def dategen():
-    return datetime.datetime(random.randint(0,3000),random.randint(1,12),random.randint(1,31))
+    return datetime.date(random.randint(0,3000),random.randint(1,12),random.randint(1,31))
+def stringgen():
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join((random.choice(letters_and_digits) for i in range(random.randint(1,10))))
+    return result_str
+print(dategen())
