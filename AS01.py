8e65a23df5cb3623895519fec0e68a6511dd6f55
adenchoy <adenchoy8@gmail.com>
2020-11-03 14:32:31 +0800

    done

---
+++ AS01.py
@@ -0,0 +1,13 @@
+def bubbleSort(arr): 
+    l = len(arr) 
+    i = 0
+    while i < l:
+        j=0
+        while j<l-1:
+            if arr[j+1] < arr[j]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            j+=1
+        i+=1
+    return arr
+
+print(bubbleSort([64, 34, 25, 12, 22, 11, 90] ))
