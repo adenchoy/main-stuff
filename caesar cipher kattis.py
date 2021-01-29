d91dc3d8b7c05919160dd148c8e83bdd995611cf
adenchoy <adenchoy8@gmail.com>
2021-01-29 12:40:23 +0800

    done

--- caesar cipher kattis.py
+++ caesar cipher kattis.py
@@ -1,10 +1,12 @@
 import random
 temp = []
+qArray = []
+aArray =[]
 numofQs = int(input("How many questions do you want?:\n"))
 
 
 with open("questionTEST.txt", "r") as whole_file:
-    for question in whole_file:
-        temp = [question.split(" : ")]
-        qArray = [x for x in temp[0::2]]
-        aArray = [y for y in temp[1::2]]
+    for line in whole_file:
+        q = line.split(" : ")
+        print(q)
+        
