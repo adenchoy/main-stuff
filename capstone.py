ec64a13062f90db8ec4262518a846aa14df924bf
adenchoy <adenchoy8@gmail.com>
2021-02-02 15:02:18 +0800

    done

---
+++ capstone.py
@@ -0,0 +1,40 @@
+import random
+temp = []
+qArray = []
+aArray =[]
+ansArray = []
+array =[]
+score = 0
+numofQs = int(input("How many questions do you want?:\n"))
+
+
+with open("questionTEST.txt", "r") as whole_file:
+    for line in whole_file:
+        q = line.split(" : ")
+        qArray.append(q[0])
+        aArray.append(q[1].strip("\n"))
+with open("answers.txt", "r") as file:
+    for line in file:
+        ansArray.append(line.strip('\n'))
+
+for i in range(numofQs):
+    qNum = random.randint(0,len(qArray)-1)
+    print(qArray[qNum])
+
+    temp=ansArray.pop(qNum)
+    array = random.sample(ansArray,3)
+    if aArray[qNum] == 'A':
+        array.insert(0,temp)
+    elif aArray[qNum] == 'B':
+        array.insert(1,temp)
+    elif aArray[qNum] == 'C':
+        array.insert(2,temp)
+    elif aArray[qNum] == 'D':
+        array.insert(3,temp)
+    print(*array, sep=', ')
+    answer = input("Input a,b,c, or d:\n")
+    if answer == aArray[qNum]:
+        score+=100
+with open("scores.txt","w") as file:
+    file.write(str(score))
+
