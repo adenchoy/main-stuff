75ce309e112a9da12210c8a83989149c1087e215
adenchoy <adenchoy8@gmail.com>
2022-01-11 13:20:34 +0800

    done

--- model.py
+++ model.py
@@ -12,11 +12,31 @@ class Model:
         self.__Other = OtherAgencies
     def __repr__(self):
         return self.__fName
-    def getter(self,sizelower, sizeupper, agelower, ageupper):
-        if self.__age >= agelower and self.__age <= ageupper and self.__dress >= sizelower and self.__dress <= sizeupper:
+    def getter(self,sizelower, sizeupper, agelower, ageupper, country):
+        if self.__age >= agelower and self.__age <= ageupper and self.__dress >= sizelower and self.__dress <= sizeupper and self.__country== country:
             return self
         else:
             pass
+    def setter(self,FirstName,LastName,Gender,Mobile,Email,Age,Country,DressSize,OtherAgencies):
+        try:
+            if Country.lower() in ["brunei", "myanmar", "cambodia", "timor-leste", "indonesia", "laos", "malaysia", "philippines", "singapore", "thailand", "vietnam"]:
+                self.__country = Country
+        except ValueError:
+            print("Not a valid country")
+        try:
+            if Gender.lower() in ["male","female","non-binary"]:
+                self.__gender = Gender
+        except ValueError:
+            print("Not a valid gender")
+        try:
+            if Gender = Male and DressSize>=46 and DressSize <=58:
+                self.__dress = DressSize
+            elif Gender = Female and DressSize>=32 and DressSize <=48:
+                self.__dress = DressSize 
+        except ValueError:
+            print("Not a valid dress size (male dress size between 46 - 58 or female dress size between 32 - 48 )")
+        
+
 
 list = []
 try:
